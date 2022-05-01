77ace10447147265770a799699a97eac36d94dbf
ogasawara kakeru <kakeru19870423@gmail.com>
2022-05-01 15:26:30 +0900

    create jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
-  );
+import React, { Component } from "react";
+// function App() {
+//   return <div>Hello, world!</div>;
+// }
+class App extends Component {
+  render() {
+    return <h1>Hello, world!</h1>;
+  }
 }
 
 export default App;
